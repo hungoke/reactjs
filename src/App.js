f42eafdc3ae03ca1f76723d4a702c0e5bf887fd8
Hung Nguyen Van <hungnvworking@gmail.com>
2021-01-13 18:35:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './scss/index.scss'
+import MusicList from './components/MusicList'
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+    console.log(this.state.count)
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Lớp học reactjs</h1>
+        <h2>count: {this.state.count}</h2>
+        <button onClick={this.increment}>Increment 1</button>
+        <div className="page test">hưng</div>
+{/*
+        <button onClick={() => this.increment(1)}>Increment 1</button>
+        <button onClick={() => this.increment(2)}>Increment 2</button> */}
+
+        <MusicList />
+        <hr />
+        <MusicList />
+        <hr />
+        <MusicList />
+        <hr />
+        <MusicList />
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
